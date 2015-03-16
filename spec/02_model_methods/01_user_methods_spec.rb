0d568e200556f5b19bcac42e756921688efb53ca
Katie Hoffman <ktahoffman@gmail.com>
2015-03-16 15:30:56 -0400

    add spec for unread and read bookmark methods in User

---
+++ spec/02_model_methods/01_user_methods_spec.rb
@@ -0,0 +1,26 @@
+describe User do
+
+  before(:all) do
+    @user = User.create(:name => "Dee", :avatar => "http://dee.jpg.to/")
+    first_article = Article.all.sample
+    second_article = Article.all.sample
+    third_article = Article.all.sample
+    @unread_bm = Bookmark.create(:user_id => @user.id, :article_id => first_article.id, :read => false)
+    first_read_bm = Bookmark.create(:user_id => @user.id, :article_id => second_article.id, :read => true)
+    second_read_bm = Bookmark.create(:user_id => @user.id, :article_id => third_article.id, :read => true)
+    @read_bookmarks = [first_read_bm, second_read_bm]
+  end
+
+  it "knows about all the articles it has read" do
+    expect { @user.read_bookmarks }.to_not raise_error
+    expect(@user.read_bookmarks.length).to eq(@read_bookmarks.length)
+    @read_bookmarks.each { |bm| expect(@user.read_bookmarks).to include(bm) }
+    expect(@user.read_bookmarks).to_not include(@unread_bm)
+  end
+
+  it "knows about all the articles it has bookmarked but hasn't read" do
+    expect { @user.unread_bookmarks }.to_not raise_error
+    expect(@user.unread_bookmarks).to include(@unread_bm)
+    @read_bookmarks.each { |bm| expect(@user.unread_bookmarks).to_not include(bm) }
+  end
+end
